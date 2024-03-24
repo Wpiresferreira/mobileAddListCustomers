56c3dfc35180c75accb838f81904c239bc39e704
Wagner Pires Ferreira <wagner_pires@icloud.com>
2024-03-24 08:46:37 -0600

    Version 1.5 - Added field validation. You can: Add a new customer. List all Customers. Remove a user from the list.

--- app/src/main/java/com/example/gla2wagnerferreira/MainActivity.java
+++ app/src/main/java/com/example/gla2wagnerferreira/MainActivity.java
@@ -1,42 +1,43 @@
 package com.example.gla2wagnerferreira;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 
 import java.lang.reflect.Type;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText firstName, lastName, birthday, phoneNumber, email;
-    private List<Customer> customerDataList;
+    DatePickerDialog datePickerDialog;
+    TextView text_Validate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         initialize();
     }
@@ -48,28 +49,116 @@ public class MainActivity extends AppCompatActivity {
         birthday = findViewById(R.id.edit_Birthday);
         phoneNumber = findViewById(R.id.edit_PhoneNumber);
         email = findViewById(R.id.edit_Email);
+        text_Validate = findViewById(R.id.text_Validate);
+
+        phoneNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                StringBuilder inputNumbers = new StringBuilder();
+                for(String str : s.toString().split("")) if (isNumber(str)) inputNumbers.append(str);
+
+                StringBuilder output = new StringBuilder();
+                String mask = "+_ (___) ___-____";
+
+                int index = 0;
+
+                for (String str : mask.split("")){
+                    if (str.equals("_") && index < inputNumbers.length()) {
+                        output.append(inputNumbers.charAt(index));
+                        index++;
+                    }else{
+                        output.append(str);
+                    }
+                }
+
+                int posCursor = 0;
+
+                for (int i = 0; i < output.length(); i++){
+                    if (isNumber(String.valueOf(output.charAt(i)))){
+                        posCursor = i;
+                    }
+                }
+
+                if(!output.toString().equals(s.toString())){
+                    phoneNumber.setText(output.toString());
+                    phoneNumber.setSelection(posCursor+1);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        birthday.setOnClickListener(v -> {
+            final Calendar c = Calendar.getInstance();
+            int mYear = c.get(Calendar.YEAR); // current year
+            int mMonth = c.get(Calendar.MONTH); // current month
+            int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+            // date picker dialog
+            datePickerDialog = new DatePickerDialog(MainActivity.this, R.style.MySpinnerDatePickerStyle,
+                    (view, year, monthOfYear, dayOfMonth) -> {
+                        // set day of month , month and year value in the edit text
+                        String displayedText = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                        birthday.setText(displayedText);
+
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+
+
+        });   // calender class's instance and get current date , month and year from calender
+
     }
 
-    public void signup(View view) {
+    private boolean isNumber(String str) {
 
+        try {
+            Integer.parseInt(str);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+
+    public void signup(View view) {
+        if (!validateFields()) {
+            return;
+        }
         Context context = this;
         SharedPreferences sharedPref = context.getSharedPreferences(
                 getString(R.string.preference_file_key), Context.MODE_PRIVATE);
 
-        Customer customer = new Customer(firstName.getText().toString(), lastName.getText().toString(),
+        Customer newCustomer = new Customer(firstName.getText().toString(), lastName.getText().toString(),
                 birthday.getText().toString(), phoneNumber.getText().toString(), email.getText().toString());
-        Type listType = new TypeToken<ArrayList<Customer>>(){}.getType();
-
+        Type listType = new TypeToken<ArrayList<Customer>>() {
+        }.getType();
 
 
         Gson gson = new Gson();
-        String json =sharedPref.getString("listusers","");
-        customerDataList = gson.fromJson(json, listType);
+        String json = sharedPref.getString("listusers", "");
+        List<Customer> customerDataList = gson.fromJson(json, listType);
 
-        if (customerDataList == null){
+        if (customerDataList == null) {
             customerDataList = new ArrayList<>();
         }
-        customerDataList.add(customer);
+
+        //Make sure there is no Customer with the same email.
+        for (Customer c : customerDataList){
+            if (c.email.equals(newCustomer.email)){
+                text_Validate.setText(R.string.this_email_is_already_registered);
+                return;
+            }
+        }
+
+        customerDataList.add(newCustomer);
 
         json = gson.toJson(customerDataList);
         SharedPreferences.Editor editor = sharedPref.edit();
@@ -83,5 +172,76 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(this, PageData.class);
         this.startActivity(myIntent);
 
+    }
+
+    private boolean validateFields() {
+
+        if (firstName.getText().length() < 2) {
+            text_Validate.setText(R.string.error_too_short);
+            firstName.requestFocus();
+            return false;
+        }
+        if (lastName.getText().length() < 2) {
+            text_Validate.setText(R.string.error_too_short);
+            lastName.requestFocus();
+            return false;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (!isValidDate(birthday.getText().toString())) {
+                text_Validate.setText(R.string.error_invalid_date);
+                birthday.requestFocus();
+                return false;
+            }
+        }
+
+        if (!isValidPhone(phoneNumber.getText().toString())){
+            text_Validate.setText(R.string.invalid_phone_number);
+            phoneNumber.requestFocus();
+            return false;
+        }
+
+
+        if (!isValidEmail(email.getText().toString())) {
+            text_Validate.setText(R.string.invalid_email);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidEmail(String string) {
+        if(string.length()<8){
+            return false;
+        }
+
+        if(!string.substring(2).contains("@")){
+            return false;
+        }
+
+        return string.substring(string.indexOf("@")).contains(".");
+    }
+
+    private boolean isValidPhone(String string) {
+
+    return !string.contains("_");
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private boolean isValidDate(String birthday) {
+
+
+        try {
+            String[] dateSeparated = birthday.split("/");
+            String dd = ("0"+dateSeparated[0]).substring(("0"+dateSeparated[0]).length()-2);
+            String MM = ("0"+dateSeparated[1]).substring(("0"+dateSeparated[1]).length()-2);
+            String yyyy = dateSeparated[2];
+
+            String dateToValidate = yyyy+"-"+MM+"-"+dd;
+            LocalDate.parse(dateToValidate);
+            return true;
+        } catch (Exception e) {
+            Log.d("IsValidDate", "isValidDate: "+ e);
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/gla2wagnerferreira/PageData.java
+++ app/src/main/java/com/example/gla2wagnerferreira/PageData.java
@@ -4,10 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,20 +12,9 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-//import com.example.gla2wagnerferreira.databinding.ActivityMainBinding;
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -45,23 +31,9 @@ public class PageData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-      //  binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_page_data);
         customerDataList = new ArrayList<>();
-
         recycler = findViewById(R.id.recycler);
-
-
-//        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-//                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(binding.navView, navController);
-
 
     loadCustomers();
     setAdapter();
@@ -94,10 +66,7 @@ public class PageData extends AppCompatActivity {
     }
 
     public void goMain(View view) {
-
         Intent myIntent = new Intent(this, MainActivity.class);
         this.startActivity(myIntent);
-
     }
-
 }
